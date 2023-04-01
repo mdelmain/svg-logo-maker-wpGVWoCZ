13ea1180b4ccf8098f0e43598256fee0dd339524
Mandy Delmaine <mandydelmaine@gmail.com>
2023-04-01 09:44:22 -0400

    updated questions and created function to create shape and a function to write to a .svg file.

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const shapes = require("./lib/shapes");
 
+const questions = [
+  {
+    type: "input",
+    name: "text",
+    message: "Enter up to three characters",
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "Enter a text color (keyword or hexadecimal number)",
+  },
+  {
+    type: "list",
+    name: "shape",
+    message: "Select a shape",
+    choices: ["circle", "triangle", "square"],
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "Enter a shape color (keyword or hexadecimal number)",
+  },
+];
+
+// function to write SVG file
+function writeToFile(data) { 
+    fs.writeFile("logo.svg", data, function (err) {
+        if (err) throw err;
+        console.info("Generated logo.svg");
+    } 
+)};
+
+function createShape(data) {
+    if (data.shape == "circle") {
+        return new shapes.Circle(data.text, data.textColor, data.shapeColor);
+    } else if (data.shape == "triangle") {
+        return new shapes.Triangle(data.text, data.textColor, data.shapeColor);
+    } else if (data.shape == "square") {
+        return new shapes.Square(data.text, data.textColor, data.shapeColor);
+    }
+};
+
+// function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        const shape = createShape(data);
+        const logo = shape.generateShape();
+        writeToFile(logo);
+    })
+}
+
+// Function call to initialize app
+init();
